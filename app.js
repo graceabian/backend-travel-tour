49602fccb9b8b51ee51823cfb6f47ca5aa6698c6
Grace Abian <graceabian10@gmail.com>
2022-03-17 16:53:18 +0800

    don set up mongo db stlas

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
+//[SECTION] Dependecies and Modules
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 
 
+
+//[SECTION] Environment Variable Setup
 dotenv.config();
 const port = process.env.PORT;
 const credentials = process.env.MONGO_URL;
 
+//[SECTION] Server Setup
 const app = express();
-    app.use(express.json())
+    app.use(express.json());
 
+//[SECTION] Database Connect
 mongoose.connect(credentials);
-	const db = mongoose.connection;
-	db.once('open', () => console.log(`Connected to Atlas`));
+const db = mongoose.connection;
+db.once('open', () => console.log(`Connected to Atlas`));
+
+//[SECTION] Server Routes	
 
 
+//[SECTION] Server Response
 
-
+app.get('/', (req, res) => {
+    res.send(`Project Deployed Successfully`);
+});
 app.listen(port, () => {
-    console.log(`API is now onlien on port ${port}`)
+    console.log(`API is now online on port ${port}`);
 });
